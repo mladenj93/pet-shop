0f97599318534471c1ec75bfa49680e2cc451d25
Mladen Jovanovic <mladen.jovanovic.edu@gmail.com>
2020-03-30 20:00:44 +0200

    TestTemplate cofiguration to work with different webdrivers

--- PetShop/src/tests/TestTemplate.java
+++ PetShop/src/tests/TestTemplate.java
@@ -8,30 +8,43 @@ import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public abstract class TestTemplate {
 	protected WebDriver driver;
 	protected Properties config;
 	protected WebDriverWait waiter;
-	
-	public void setupDriverAndWaiter() {
-		System.setProperty("webdriver.chrome.driver", "driver-lib\\chromedriver.exe");
-		this.driver = new ChromeDriver();
+
+	public void setupDriverAndWaiter(String browser) throws Exception {
+		if (browser.equalsIgnoreCase("firefox")) {
+			System.setProperty("webdriver.gecko.driver", "driver-lib\\geckodriver.exe");
+			driver = new FirefoxDriver();
+		} else if (browser.equalsIgnoreCase("chrome")) {
+			System.setProperty("webdriver.chrome.driver", "driver-lib\\chromedriver.exe");
+			driver = new ChromeDriver();
+		} else if (browser.equalsIgnoreCase("Edge")) {
+			System.setProperty("webdriver.edge.driver", "driver-lib\\msedgedriver.exe");
+			driver = new EdgeDriver();
+		} else {
+			throw new Exception("Browser is not correct");
+		}
+
 		driver.manage().window().maximize();
-        driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
+		driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-		
-		this.waiter = new WebDriverWait(driver,30);
+
+		this.waiter = new WebDriverWait(driver, 30);
 	}
-	
+
 	public void loadConfig() throws Exception {
-		this.config =  new Properties();
+		this.config = new Properties();
 		config.load(new FileInputStream("config/config.properties"));
 	}
-	
-	public void init() throws Exception {
-		this.setupDriverAndWaiter();
+
+	public void init(String browser) throws Exception {
+		this.setupDriverAndWaiter(browser);
 		this.loadConfig();
 	}
 
